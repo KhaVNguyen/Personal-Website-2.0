bb6385f315868a6aad7f1f77f32929aeb871cf16
Kha Nguyen <kha.vng@gmail.com>
2018-08-09 19:44:11 -0700

    Implemented close on click outside card

--- js/expander.js
+++ js/expander.js
@@ -1,4 +1,5 @@
 var $cell = $('.card');
+var $currentlyExpanded;
 
 // On click on card, open / close the card
 $cell.find('.js-expander').click(function() {
@@ -8,18 +9,23 @@ $cell.find('.js-expander').click(function() {
   if ($thisCell.hasClass('is-collapsed')) {
     $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed');
     $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+    $currentlyExpanded = $(this).closest('.card');
   } else {
     $thisCell.removeClass('is-expanded').addClass('is-collapsed');
     $cell.not($thisCell).removeClass('is-inactive');
   }
 });
 
-//close card when click on cross
+// Close card when click on cross
 $cell.find('.js-collapser').click(function() {
-
   var $thisCell = $(this).closest('.card');
-
   $thisCell.removeClass('is-expanded').addClass('is-collapsed');
-  $cell.not($thisCell).removeClass('is-inactive');
+});
 
+// Close card when clicking outside of card
+$(document).click(function(event) { 
+    if(!$(event.target).closest('.card').length) {
+       $currentlyExpanded.removeClass('is-expanded').addClass('is-collapsed');
+        console.log("Clicked outside of card");
+    }        
 });
