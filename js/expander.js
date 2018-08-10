251a4a8e3e6d05bf03007bcf58ea8011fbd3613b
Kha Nguyen <kha.vng@gmail.com>
2018-08-09 18:43:33 -0700

    Added arrow (only left) to cards

--- js/expander.js
+++ js/expander.js
@@ -1,20 +1,13 @@
 var $cell = $('.card');
 
-//open and close card when clicked on card
+// On click on card, open / close the card
 $cell.find('.js-expander').click(function() {
 
   var $thisCell = $(this).closest('.card');
 
   if ($thisCell.hasClass('is-collapsed')) {
-    $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
+    $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed');
     $thisCell.removeClass('is-collapsed').addClass('is-expanded');
-    
-    if ($cell.not($thisCell).hasClass('is-inactive')) {
-      //do nothing
-    } else {
-      $cell.not($thisCell).addClass('is-inactive');
-    }
-
   } else {
     $thisCell.removeClass('is-expanded').addClass('is-collapsed');
     $cell.not($thisCell).removeClass('is-inactive');
